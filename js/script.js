d9ed4ef24aaa7fdad49ddb47dcbf7ff5a737fc4e
KalysbekovDev <tugolbaevkalys@gmail.com>
2022-09-11 14:25:39 +0600

    main

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// swiper js code
+const swiper = new Swiper(".home-slider ", {
+    loop:true,
+    grabCursor:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
